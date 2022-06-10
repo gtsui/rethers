3b435ea1b893feab2416a3f37864e34ba3485551
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2022-06-10 15:35:14 +0800

    add more env conversions

--- ethers-utils/src/env.rs
+++ ethers-utils/src/env.rs
@@ -11,6 +11,11 @@ pub fn env_key_prefixed_U256(key: &str) -> U256 {
   str_to_U256(&prefixed_key)
 }
 
+pub fn env_key_prefixed_u64(key: &str) -> u64 {
+  let prefixed_key = env_key_prefixed(key);
+  prefixed_key.parse::<u64>()
+}
+
 pub fn env_key_prefixed_H160(key: &str) -> H160 {
   let prefixed_key = env_key_prefixed(key);
   str_to_H160(&prefixed_key)
@@ -19,6 +24,11 @@ pub fn env_key_prefixed_H160(key: &str) -> H160 {
 pub fn env_key_U256(key: &str) -> U256 {
   let key = dotenv::var(key).unwrap();
   str_to_U256(&key)
+}
+
+pub fn env_key_u64(key: &str) -> u64 {
+  let key = dotenv::var(key).unwrap();
+  key.parse::<u64>()
 }
 
 pub fn env_key_H160(key: &str) -> H160 {

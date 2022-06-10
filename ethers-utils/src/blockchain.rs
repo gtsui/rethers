c654266b9d1fb97c8f5dc9dce5ef7972207f1fa5
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2022-06-11 02:00:06 +0800

    add console logging

--- ethers-utils/src/blockchain.rs
+++ ethers-utils/src/blockchain.rs
@@ -15,6 +15,7 @@ pub trait MempoolListener {
 
     let mut stream = provider.subscribe_pending_txs().await.unwrap();
 
+    println!("Listening to mempool...");
     while let Some(tx_hash) = stream.next().await {
       let maybe_tx = provider.get_transaction(tx_hash).await.unwrap();
       if let Some(tx) = maybe_tx {

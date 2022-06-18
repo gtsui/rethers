f73e5971aa6d3bf2e3f7819e69ece6744ef8a7e4
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2022-06-18 15:48:28 +0800

    added seconds to timestamp format

--- ethers-utils/src/time.rs
+++ ethers-utils/src/time.rs
@@ -4,7 +4,7 @@ use std::time::{UNIX_EPOCH, Duration};
 pub fn fmt_timestamp(timestamp: u64) -> String {
   let d = UNIX_EPOCH + Duration::from_secs(timestamp);
   let datetime = DateTime::<Local>::from(d);
-  datetime.format("%Y-%m-%d %H:%M").to_string()  
+  datetime.format("%Y-%m-%d %H:%M:%S").to_string()  
 }
 
 pub fn current_time() -> u64 {

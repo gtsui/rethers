6defb2940f29f6703834f3f01bcc3dd8cf3136c1
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2022-06-25 14:25:12 +0800

    clean up example

--- examples/sample_telegram.rs
+++ examples/sample_telegram.rs
@@ -3,26 +3,8 @@ use ethers_utils::*;
 #[tokio::main]
 async fn main() {
 
-  let msg = format!("https://moonriver.moonscan.io/tx/{}", "0x7dbb4b1ed73ea2b6f4dd8ac27fc1f812c62fdb4a07715c2d09bde47af0bb7e07");
+  let msg = format!("https://etherscan.io/tx/{}", "0x764e6df1bdcafc97b720e34095d199414a5567693b844a49022cf7318f6e0c06");
   
   telegram_alert(&env_key_str("TELEGRAM_BOT_TOKEN"), -403280594, &msg).await;
-
-  
-  // let bot = Bot::new(env_key_str("TELEGRAM_BOT_TOKEN")).auto_send();
-  // let id = teloxide::types::Recipient::Id(ChatId(-403280594));
-
-  // let res = bot.send_message(id, "whatsup madafaka").await.unwrap();
-
-  // println!("{:?}", res);
-  
-  // teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-
-  //   //println!("{:?}", message.chat.id);
-  //   let id = teloxide::types::Recipient::Id(ChatId(-403280594));
-  //   bot.send_message(id, "hello world!").await?;
-  //   //bot.send_dice(id).await?;
-  //   respond(())
-  // })
-  //   .await;
-  
+    
 }

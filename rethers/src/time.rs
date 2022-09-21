0ddca9b3687f56b800a3b390450146c3987d05c1
Geoffrey Tsui <tsui.geoffrey@gmail.com>
2022-09-21 10:23:51 +0800

    add current_time_millis() fn

--- rethers/src/time.rs
+++ rethers/src/time.rs
@@ -10,6 +10,11 @@ pub fn current_time_secs() -> u64 {
   time.duration_since(UNIX_EPOCH).unwrap().as_secs()  
 }
 
+pub fn current_time_millis() -> u128 {
+  let time = std::time::SystemTime::now();
+  time.duration_since(UNIX_EPOCH).unwrap().as_millis()
+}
+
 pub fn fmt_timestamp(timestamp: u64) -> String {
   let d = UNIX_EPOCH + Duration::from_secs(timestamp);
   let datetime = DateTime::<Local>::from(d);
